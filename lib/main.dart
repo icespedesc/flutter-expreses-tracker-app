a590ae5e2b0bd91a731cf654d44d390ca422310c
icespedes@check.cl <icespedes@check.cl>
2025-02-02 20:15:31 -0300

    Add dark theme support with customized styles for app bar, cards, and buttons

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,46 @@ import 'package:expense_tracker/widgets/expenses.dart';
 
 var kColorScheme =
     ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 76, 172, 84));
+var kDarkScheme = ColorScheme.fromSeed(
+    brightness: Brightness.dark,
+    seedColor: const Color.fromARGB(255, 6, 51, 10));
 
 void main() {
   runApp(
     MaterialApp(
+      darkTheme: ThemeData.dark().copyWith(
+        colorScheme: kDarkScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kDarkScheme.onPrimaryContainer,
+          foregroundColor: kDarkScheme.primaryContainer,
+        ),
+        cardTheme: CardTheme().copyWith(
+          color: kDarkScheme.primaryContainer,
+          margin: const EdgeInsets.symmetric(
+            vertical: 8,
+            horizontal: 16,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kDarkScheme.primaryContainer,
+            foregroundColor: kDarkScheme.onPrimaryContainer,
+            textStyle: TextStyle(
+                color: kDarkScheme.secondaryContainer,
+                fontWeight: FontWeight.bold),
+          ),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+            titleLarge: TextStyle(fontWeight: FontWeight.bold, fontSize: 14),
+            labelMedium: TextStyle(
+                color: Colors.white, fontSize: 16, fontWeight: FontWeight.bold),
+            bodyMedium: TextStyle(color: Colors.white)),
+      ),
       theme: ThemeData().copyWith(
         colorScheme: kColorScheme,
         appBarTheme: const AppBarTheme().copyWith(
           backgroundColor: kColorScheme.onPrimaryContainer,
-          foregroundColor: kColorScheme.primaryContainer,
+          foregroundColor: kColorScheme.secondaryContainer,
         ),
         cardTheme: CardTheme().copyWith(
           color: kColorScheme.tertiaryContainer,
@@ -38,6 +69,8 @@ void main() {
                 fontSize: 16,
                 fontWeight: FontWeight.bold)),
       ),
+
+      //themeMode: ThemeMode.system, By default.
       home: Expenses(),
     ),
   );
