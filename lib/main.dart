f95a1c03210803440b345173647a0b77838b1fa8
icespedes@check.cl <icespedes@check.cl>
2025-02-02 19:56:33 -0300

    Enhance UI with custom theme and improve expense item display in ExpensesList

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/widgets/expenses.dart';
 
+var kColorScheme =
+    ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 76, 172, 84));
+
 void main() {
   runApp(
     MaterialApp(
+      theme: ThemeData().copyWith(
+        colorScheme: kColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+        cardTheme: CardTheme().copyWith(
+          color: kColorScheme.tertiaryContainer,
+          margin: const EdgeInsets.symmetric(
+            vertical: 8,
+            horizontal: 16,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.primaryContainer,
+            textStyle: TextStyle(
+                color: kColorScheme.secondaryContainer,
+                fontWeight: FontWeight.bold),
+          ),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+            titleLarge: TextStyle(
+                color: kColorScheme.onSecondaryContainer,
+                fontWeight: FontWeight.bold,
+                fontSize: 14),
+            labelMedium: TextStyle(
+                color: Colors.white,
+                fontSize: 16,
+                fontWeight: FontWeight.bold)),
+      ),
       home: Expenses(),
     ),
   );
--- lib/widgets/expenses_list/expenses_list.dart
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -2,23 +2,59 @@ import 'package:expense_tracker/models/expense.dart';
 import 'package:expense_tracker/widgets/expenses_list/expense_item.dart';
 import 'package:flutter/material.dart';
 
-class ExpensesList extends StatelessWidget {
+class ExpensesList extends StatefulWidget {
   const ExpensesList(
       {super.key, required this.expenses, required this.onRemoveExpense});
 
   final List<Expense> expenses;
   final void Function(Expense expense) onRemoveExpense;
+  @override
+  State<ExpensesList> createState() => _ExpensesList();
+}
 
+class _ExpensesList extends State<ExpensesList> {
+  double _iconOffset = 0.0;
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      itemCount: expenses.length,
+      itemCount: widget.expenses.length,
       itemBuilder: (ctx, index) => Dismissible(
-        key: ValueKey(expenses[index]),
-        onDismissed: (direction) {
-          onRemoveExpense(expenses[index]);
+        key: ValueKey(widget.expenses[index]),
+        onUpdate: (details) {
+          setState(() {
+            _iconOffset =
+                details.progress * 100; // Adjust the multiplier as needed
+          });
         },
-        child: ExpenseItem(expense: expenses[index]),
+        background: Container(
+          alignment: Alignment.centerRight,
+          margin: EdgeInsets.symmetric(
+              horizontal: Theme.of(context).cardTheme.margin!.horizontal),
+          decoration: BoxDecoration(
+              color:
+                  Theme.of(context).colorScheme.error.withValues(alpha: 0.75),
+              borderRadius: BorderRadius.circular(3)),
+          child: Align(
+            alignment: Alignment.center,
+            child: Transform.translate(
+              offset: Offset(_iconOffset, 0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.delete, color: Colors.white),
+                  Text(
+                    'Remove',
+                    style: Theme.of(context).textTheme.copyWith().labelMedium
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+        onDismissed: (direction) {
+          widget.onRemoveExpense(widget.expenses[index]);
+        },
+        child: ExpenseItem(expense: widget.expenses[index]),
       ),
     );
   }
